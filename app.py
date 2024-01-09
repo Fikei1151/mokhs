5d8064637f1c1a73ae67e8cc19192682191da312
Fikree Hajiyusof <fikree205m@gmail.com>
2024-01-09 10:25:13 +0700

    add css js bootstap

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to the Grade Submission and Viewing System!'
+   return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=8000)
